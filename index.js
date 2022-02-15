ad0060bf30e615bda3f59e8b989d9bb8c467bf52
Jennifer Stewart <jstewart8053@gmail.com>
2022-02-15 12:29:12 -0600

    added index.html file and style for later use displaying scrapped data.

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import request from 'request';
-// import required
-import cheerio from 'cheerio';
-import fs from 'fs';
+
+//
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 
-const app = express()
+const markup = `
+<ul class="fruits">
+  <li class="fruits__mango"> Mango </li>
+  <li class="fruits__apple"> Apple </li>
+</ul>
+`;
 
-// let title, release, rating
+const $ = cheerio.load(markup)
+const mango = $(".fruits__mango");
+const apple = $(".fruits__apple");
 
-// app.get('/scrape', (req, res) => {
-  
 
-// scrapping code
-let url = "https://www.imdb.com/title/tt0145487/"
-
-// request(url, function (err, response, html) {
-
-//     if (err) return console.error(err);
-//     let $ = cheerio.load(html)
-   
- 
-//    title = $("TitleHeader__TitleText-sc-1wu6n3d-0 dxSWFG").text()
-       
-        
-//         console.log(title);
-
-//     })
-// })
-
-const main = async () => {
-    const response = await fetch(url)
-    const body = await response.text()
-    console.log(body)
-}
-
-app.listen(5000, function() {
- console.log("Server running on port 5000")
-})
-
+console.log(apple.attr("class"))
+console.log(mango.html())
+console.log($.html())
